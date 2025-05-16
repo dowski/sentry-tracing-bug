4f05efc88b13a4d5ca27feab1781c85e5bf67370
Christian Wyglendowski <christian@dowski.com>
2025-05-16 16:16:07 -0400

    An Astro app configured to use Sentry + tracing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sentry({
+      dsn: "https://8c4420ff673c5b887fbc1831a036e599@o4509333385314304.ingest.us.sentry.io/4509334607167488",
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      // Setting this option to true will send default PII data to Sentry.
+      // For example, automatic IP address collection on events
+      sendDefaultPii: true,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+});
